e76235c04894c17ff441d5e49f50aac8c42bf3f6
Charizard17 <3723esad@gmail.com>
2023-06-15 19:59:55 +0200

    initial commit

---
+++ Yumfinity/YumfinityApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumfinityApp.swift
+//  Yumfinity
+//
+//  Created by Esad Dursun on 15.06.23.
+//
+
+import SwiftUI
+
+@main
+struct YumfinityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
